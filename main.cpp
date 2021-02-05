4137970d60583289c278cde6d186538b4abb8921
Tomas Sinkunas <Tomas@rendertom.com>
2021-02-05 18:25:10 +0200

    modify frame size based on user input value

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
 
+bool isEdge(int i, int rowCount, int j, int columnCount) {
+    return i == 0 || i == rowCount - 1 || j == 0 || j == columnCount - 1;
+}
+
 bool isMale(std::string name) {
     const int stringLenght(name.length());
     const char lastCharacter(name[stringLenght - 1]);
@@ -8,24 +12,33 @@ bool isMale(std::string name) {
     return tolower(lastCharacter) == 's';
 }
 
-void PrintFrame(std::string name) {
+void PrintFrame(std::string name, int marginSize = 0) {
     const std::string greeting(isMale(name) ? "Sveikas" : "Sveika");
     const std::string sentence(greeting + ", " + name + "!");
+    if (marginSize == 0) {
+        std::cout << sentence << std::endl;
+        return;
+    }
+
+    const char edge('*');
+    const char space(' ');
     const int sentenceLength(sentence.length());
+    const int marginSizeDouble(marginSize * 2);
+    const int rowCount(marginSizeDouble + 1);
+    const int columnCount(sentenceLength + marginSizeDouble);
 
-    const std::string preffix("* ");
-    const std::string suffix(" *");
-    const std::string line1(preffix.length() + sentenceLength + suffix.length(), '*');
-    const std::string line2(preffix + std::string(sentenceLength, ' ') + suffix);
-    const std::string line3(preffix + sentence + suffix);
-    const std::string line4(line2);
-    const std::string line5(line1);
+    for (int i = 0; i < rowCount; i++) {
+        for (int j = 0; j < columnCount; j++) {
+            if (i == marginSize && j == marginSize) {
+                std::cout << sentence;
+                j += sentenceLength - 1;
+            } else {
+                std::cout << (isEdge(i, rowCount, j, columnCount) ? edge : space);
+            }
+        }
 
-    std::cout << line1 << std::endl;
-    std::cout << line2 << std::endl;
-    std::cout << line3 << std::endl;
-    std::cout << line4 << std::endl;
-    std::cout << line5 << std::endl;
+        std::cout << std::endl;
+    }
 }
 
 int main() {
@@ -33,7 +46,21 @@ int main() {
     std::cout << "Please enter your name: ";
     std::cin >> name;
 
-    PrintFrame(name);
+    const int minValue = 0;
+    const int maxValue = 10;
+
+    int marginSize;
+    std::cout << "Please enter margin size (" << minValue << "-" << maxValue << "): ";
+    std::cin >> marginSize;
+
+    if (marginSize < 0 || marginSize > 10) {
+        std::cout << "Error: margin size " << marginSize
+                  << " is out of range (" << minValue << "-" << maxValue << ")"
+                  << std::endl;
+        exit(1);
+    }
+
+    PrintFrame(name, marginSize);
 
     return 0;
 }
